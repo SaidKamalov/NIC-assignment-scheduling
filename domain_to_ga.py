fcf53830ded1680e4deec5a1283be019b6a4d2b7
SaidKamalov <arturrulit5@gmail.com>
2023-05-03 23:06:13 +0300

    clean and refactor

--- domain_to_ga.py
+++ domain_to_ga.py
@@ -17,13 +17,11 @@ class AssignmentGene:
         self.assignment: Assignment = assignment
         self.start: date = deepcopy(self.assignment.start_date)
         self.deadline: date = deepcopy(self.assignment.end_date)
-        self.score: float = 0
 
     def __str__(self) -> str:
         res = str(self.assignment) + "\n"
         res += f"suggested slot: from {self.start} to {self.deadline}.\n"
         res += f"It is {(self.deadline - self.start).days} days for {self.assignment.hours_to_complete} hours of work.\n"
-        res += f"score is {self.score}"
         return res
 
     def __lt__(self, other) -> bool:
@@ -53,7 +51,6 @@ def generate_chromo(genes: list[AssignmentGene]) -> list[AssignmentGene]:
     genes_for_chromo: list[AssignmentGene] = [
         generate_rand_gene(gene) for gene in genes
     ]
-    # chromo: Chromosome = Chromosome(genes_for_chromo)
     return genes_for_chromo
 
 
@@ -65,21 +62,6 @@ def generate_rand_gene(gene: AssignmentGene) -> AssignmentGene:
     new_gene = AssignmentGene(gene.assignment)
     new_gene.start, new_gene.deadline = new_gene.rand_time_slot()
     return new_gene
-
-
-def random_mutate_time_slot(
-    population: list[Chromosome], mutation_rate: float, *args
-) -> list[Chromosome]:
-    """
-    Function to mutate population.
-    """
-    for i in range(len(population)):
-        for j in range(len(population[i].genes)):
-            if random() < mutation_rate:
-                population[i].genes[j].start_date, population[i].genes[j].end_date = (
-                    population[i].genes[j].rand_time_slot()
-                )
-    return population
 
 
 def random_mutate_genes(
@@ -132,23 +114,3 @@ if __name__ == "__main__":
     print()
     fitness_score = fitness(genes=genes_for_chromo)
     print("fitness sore of chromo", fitness_score)
-
-    # from input_handling.input import _generate_assignments
-
-    # assignments = [Assignment(**ass) for ass in _generate_assignments(5)]
-    # genes = [AssignmentGene(ass) for ass in assignments]
-    # print("initial genes:")
-    # for gene in genes:
-    #     print(gene)
-    # print()
-    # print("generated chromo:")
-    # chromo = generate_chromo(genes)
-    # for gene in chromo.genes:
-    #     print(gene)
-    # print()
-    # print("check initial genes:")
-    # for gene in genes:
-    #     print(gene)
-    # print()
-    # print("fitness score")
-    # print(Fitness.fitness(chromo.genes))
