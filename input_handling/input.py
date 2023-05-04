f24e0e62f9d09f096fac23707de0df0d2f5065ae
SaidKamalov <arturrulit5@gmail.com>
2023-05-04 16:36:38 +0300

    refactor schedule import

--- input_handling/input.py
+++ input_handling/input.py
@@ -2,6 +2,7 @@ import json
 import datetime
 from datetime import date, timedelta
 from random import randint, random
+import environment
 
 ASSIGNMENT_FIELDS = {
     "name",
@@ -102,6 +103,32 @@ def _generate_assignments(
         )
 
         yield assignment
+
+
+def get_number_of_classes_per_day(study_year: str, track: str) -> dict[str, int] | None:
+    """
+    Returns the number of classes per day for each day of the week.
+    """
+    number_of_classes = json.load(open(environment.PATH_TO_SCHEDULE))
+    return number_of_classes.get(study_year, {}).get(track, None)
+
+
+def get_free_hours_per_day(study_year: str, track: str) -> dict[str, float] | None:
+    """
+    Returns the number of free hours per day for each day of the week.
+    """
+    number_of_classes = get_number_of_classes_per_day(study_year, track)
+    if number_of_classes is None:
+        return None
+    free_hours = {}
+    for day, number in number_of_classes.items():
+        # TODO: determine the suitable number of studying hours on weekends
+        if day in ["5", "6"]:
+            free_hours[day] = 6 - number * 1.5
+        else:
+            free_hours[day] = 10 - number * 1.5
+
+    return free_hours
 
 
 if __name__ == "__main__":
